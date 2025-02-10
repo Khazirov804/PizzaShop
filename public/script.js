f102ffc74c02b0b4ddbc967a5ae4a331f8d07240
Dinislam <khazirov804@mail.ru>
2025-02-10 20:12:56 +0300

    Создали заглушку добавить в корзину

--- public/script.js
+++ public/script.js
@@ -1,9 +1,13 @@
 function something() {
   var x = window.localStorage.getItem("bbb");
 
-  x = x * 1 + 1;
+  x = x ? parseInt(x, 10) + 1 : 1; // Проверка на null
 
   window.localStorage.setItem("bbb", x);
 
   alert(x);
+}
+
+function add_to_cart() {
+  alert("Hello from function");
 }
